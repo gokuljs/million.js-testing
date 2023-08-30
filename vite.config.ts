3b360cf29b308d3a7ebc4a040b7e382bc764f8f7
gokuljs <gokuljs029@gmail.com>
2023-08-30 22:17:06 +0530

    test

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
+import eslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr(), checker({ typescript: true }), eslint()],
+  server: {
+    port: 3000,
+    open: true,
+    host: '127.0.0.1',
+  },
+});
