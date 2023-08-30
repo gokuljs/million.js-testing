52f4a52522f5d31a6940eaa60c5f02c1353f715a
gokuljs <gokuljs029@gmail.com>
2023-08-31 05:18:11 +0530

    million config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
+import million from 'million/compiler';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), checker({ typescript: true }), eslint()],
+  plugins: [million.vite({ auto: true }),react(), svgr(), checker({ typescript: true }), eslint()],
   server: {
     port: 3000,
     open: true,
